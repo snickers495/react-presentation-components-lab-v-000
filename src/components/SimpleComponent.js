1824913c826189fd75604de6818737df60755711
Lisa Lee <snickers495@gmail.com>
2018-02-07 15:42:11 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -8,7 +8,7 @@ export default class SimpleComponent extends Component {
     }
   }
   handleClick = (){
-    this.state.mood === 'happy' ? this.setState({mood: 'sad'}) : 
+    this.state.mood === 'happy' ? this.setState({mood: 'sad'}) : this.setState
   }
   render(){
     return(
