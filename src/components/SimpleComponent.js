6cd1fd82bbdfb44870a33d8d979e3d0f1c52a9a6
Lisa Lee <snickers495@gmail.com>
2018-02-07 15:40:04 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -2,6 +2,9 @@
 import React, {Component} from 'react'
 export default class SimpleComponent extends Component {
   constructor(){
-    super(),
+    super()
+    this.state = {
+      
+    }
   }
 }
